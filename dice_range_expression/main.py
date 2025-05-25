5f37718aca1c130a6eeefe325ee3cfa632476adf
unknown <vince2010.kerti@gmail.com>
2025-05-25 21:35:59 +0200

    first commit

--- dice_range_expression/main.py
+++ dice_range_expression/main.py
@@ -1,4 +1,32 @@
 with open('./input.txt', 'r') as f:
   input = f.read()
 
-print(input)
+dices = {
+  2: 'd2',
+  3: 'd3',
+  4: 'd4',
+  6: 'd6',
+  8: 'd8',
+  10: 'd10',
+  20: 'd20'
+}
+
+def find_dice(a, b):
+  main_offset = a if a == 1 else a - 1
+  interval_nums = b - a + 1
+  multiplyer = 1
+  if a == 1:
+    for d in dices.keys():
+      if d == b:
+        print(f"{multiplyer}{dices[d]} + {main_offset}")
+  for d in dices.keys():
+    # check if it's possible with 1 dice
+    if (a == 1 and b == d) or d + main_offset == b:
+      print(f"{multiplyer}{dices[d]} + {main_offset}")
+      break
+
+
+
+find_dice(1, 4)
+
+#print(input)
--- fibonacci/main.py
+++ fibonacci/main.py
@@ -1,4 +1,36 @@
 with open('./input.txt', 'r') as f:
   input = f.read()
 
-print(input)
+lines = input.split('\n')
+
+#generates fibonacci numbers
+def fibonacci_if_divisible(max_value):
+  fibonacci_nums = [0, 1]
+  divisible_fibonacci = [0]
+  while fibonacci_nums[len(fibonacci_nums) - 1] + fibonacci_nums[len(fibonacci_nums) - 2] <= max_value:
+    current = (fibonacci_nums[len(fibonacci_nums) - 1] + fibonacci_nums[len(fibonacci_nums) - 2])
+    fibonacci_nums.append(current)
+    if current % 3 == 0:
+      divisible_fibonacci.append(current)
+  return divisible_fibonacci
+
+def main_function():
+  for line in lines:
+    # input validation
+    if line.isdigit() == False:
+      print('N/A')
+    else:
+      nums = fibonacci_if_divisible(int(line))
+      # no solution
+      if len(nums) == 0:
+        print('N/A')
+      else:
+        for n in range(len(nums)):
+          #we don't put a comma to the end of the line
+          if n < len(nums) - 1:
+            print(nums[n], end=", ")
+          else:
+            print(nums[n])
+
+main_function()
+
